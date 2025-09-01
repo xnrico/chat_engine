32e726a9448e8a87471086334bb7d18a633b7108
Rico Xu <rico.xu@srd.subaru.co.jp>
2025-09-01 11:15:58 -0400

    protobuf compiler works

--- src/chat_engine.cpp
+++ src/chat_engine.cpp
@@ -1,10 +1,7 @@
 #include <stdio.h>
-#include <rpc/client.h>
-
 #include <tinyfsm/tinyfsm.hpp>
 
 auto main(int argc, char* argv[]) -> int {
-  auto client = rpc::client("localhost", 10001);
   printf("Hello, Chat Engine!\n");
   return 0;
 }
